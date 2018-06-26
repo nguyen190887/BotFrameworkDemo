ccdac1d8a4495cd90a75ca94fe16ff82c7a174a6
nguyen190887 <nguyen190887@gmail.com>
2018-06-26 16:23:41 +0700

    Try with prompt

--- BotFrameworkDemo/Dialogs/SurveyDialog.cs
+++ BotFrameworkDemo/Dialogs/SurveyDialog.cs
@@ -8,49 +8,73 @@ using System.Web;
 
 namespace BotFrameworkDemo.Dialogs
 {
+    //[Serializable]
+    //public class SurveyDialog : IDialog<object>
+    //{
+    //    public async Task StartAsync(IDialogContext context)
+    //    {
+    //        await context.PostAsync("Hello, I'm the survey dialog. I'm interrupting your conversation to ask you a question. Type \"done\" to resume");
+
+    //        context.Wait(this.MessageReceivedAsync);
+    //    }
+
+    //    public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+    //    {
+    //        var activity = await result as Activity;
+
+    //        var withoutMentions = GetTextWithoutMentions(activity);
+    //        await context.PostAsync($"Before: {activity.Text} - After: {withoutMentions}");
+
+    //        if (withoutMentions == "done")
+    //        {
+    //            await context.PostAsync("Great, back to the original conversation!");
+    //            context.Done(String.Empty); //Finish this dialog
+    //        }
+    //        else
+    //        {
+    //            await context.PostAsync("I'm still on the survey until you type \"done\"");
+    //            context.Wait(MessageReceivedAsync); //Not done yet
+    //        }
+    //    }
+
+    //    private string GetTextWithoutMentions(Activity activity)
+    //    {
+    //        string output = activity.Text;
+
+    //        // remove mentions
+    //        foreach (var mention in activity.GetMentions())
+    //        {
+    //            output = output.Replace(mention.Text, "");
+    //        }
+
+    //        // remove @botname
+    //        output = output.Replace($"@{activity.Recipient.Name}", "");
+
+    //        return output.Trim();
+    //    }
+    //}
+
     [Serializable]
     public class SurveyDialog : IDialog<object>
     {
         public async Task StartAsync(IDialogContext context)
         {
-            await context.PostAsync("Hello, I'm the survey dialog. I'm interrupting your conversation to ask you a question. Type \"done\" to resume");
-
-            context.Wait(this.MessageReceivedAsync);
+            PromptDialog.Choice(context, this.AfterSelectOption, new string[] { "Stay in this survey", "Get back to where I was" }, "Hello, you're in the survey dialog. Please pick one of these options");
         }
 
-        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        private async Task AfterSelectOption(IDialogContext context, IAwaitable<string> result)
         {
-            var activity = await result as Activity;
 
-            var withoutMentions = GetTextWithoutMentions(activity);
-            await context.PostAsync($"Before: {activity.Text} - After: {withoutMentions}");
-
-            if (withoutMentions == "done")
+            if ((await result) == "Get back to where I was")
             {
                 await context.PostAsync("Great, back to the original conversation!");
                 context.Done(String.Empty); //Finish this dialog
             }
             else
             {
-                await context.PostAsync("I'm still on the survey until you type \"done\"");
-                context.Wait(MessageReceivedAsync); //Not done yet
+                await context.PostAsync("I'm still on the survey until you tell me to stop");
+                PromptDialog.Choice(context, this.AfterSelectOption, new string[] { "Stay in this survey", "Get back to where I was" }, "Hello, you're in the survey dialog. Please pick one of these options");
             }
-        }
-
-        private string GetTextWithoutMentions(Activity activity)
-        {
-            string output = activity.Text;
-
-            // remove mentions
-            foreach (var mention in activity.GetMentions())
-            {
-                output = output.Replace(mention.Text, "");
-            }
-
-            // remove @botname
-            output = output.Replace($"@{activity.Recipient.Name}", "");
-
-            return output.Trim();
         }
     }
 }
